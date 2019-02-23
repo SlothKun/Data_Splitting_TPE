497bd83dde5724f7ca11292d276535d4c75a8e5f
Sloth-Kun <slothkun00@gmail.com>
2019-02-23 03:50:19 +0100

    Add files via upload
    peaufinage, mais reste quelques bugs

---
+++ Server/Server2/Main_Server.py
@@ -0,0 +1,191 @@
+import Objects_Server
+
+# Create Conn Objects
+Client1_conn = Objects_Server.Server()
+Client2_conn = Objects_Server.Server()
+
+# Create DH_algo Objects
+DH_Algorithm_Client1 = Objects_Server.DH_algorithm()
+DH_Algorithm_Client2 = Objects_Server.DH_algorithm()
+
+# Create Key Objects
+KeyFile_Client1 = Objects_Server.Key()
+KeyFile_Client2 = Objects_Server.Key()
+
+# Create File and AES objects
+AES_Encryption = Objects_Server.AES_Algorithm()
+File_Manipulation = Objects_Server.File()
+
+# Create some variable
+global danger
+global key_initialised
+danger = False
+receiving_sending_mode = 0
+c1_connected = False
+c2_connected = False
+dh_initialised = False
+key_initialised = False
+
+# Create function to clear the code
+def conn_s(client):
+    if client == 1:
+        Client1_conn.server_activation()
+        print("Client 1 connected")
+        return True
+    elif client == 2:
+        Client2_conn.server_activation()
+        print("Client 2 connected")
+        return True
+
+def dh_init():
+    dh_pubkey = []
+    dh_pbkey_c1 = DH_Algorithm_Client1.public_key_generator()
+    dh_pbkey_c2 = DH_Algorithm_Client2.public_key_generator()
+    if len(dh_pubkey) == 0:
+        dh_pubkey.append(Client1_conn.receiving())
+        Client1_conn.sending(dh_pbkey_c1)
+    elif len(dh_pubkey) == 1:
+        dh_pubkey.append(Client2_conn.receiving())
+        Client2_conn.sending(dh_pbkey_c2)
+    elif len(dh_pubkey) == 2:
+        dh_part_key_C1 = Client1_conn.receiving()
+        dh_part_key_C2 = Client2_conn.receiving()
+        Client1_conn.sending(dh_part_key_C2)
+        Client2_conn.sending(dh_part_key_C1)
+        dh_pubkey.append((dh_part_key_C1 + dh_part_key_C2))
+    elif len(dh_pubkey) == 3:
+        DH_Algorithm_Client1.private_key_generator(dh_pubkey[0])
+        DH_Algorithm_Client2.private_key_generator(dh_pubkey[1])
+        return False, True
+
+def key_init():
+    big_key_nonce = []
+    if len(big_key_nonce) == 0:
+        big_key_nonce.append(DH_Algorithm_Client1.decrypt(Client1_conn.receiving()))
+        key_nonce_sum, key_nonce = KeyFile_Client1.get_big_key_nonce(0, big_key_nonce[0])
+        if not File_Manipulation.file_integrity_check(key_nonce, key_nonce_sum):
+            integrity_failed_closing_protocol("Integrity fail.")
+        else:
+            if data_check(key_nonce) == "ok":
+                KeyFile_Client1.get_big_key_nonce(1, key_nonce)
+    elif len(big_key_nonce) == 1:
+        big_key_nonce.append(DH_Algorithm_Client2.decrypt(Client2_conn.receiving()))
+        key_nonce_sum, key_nonce = KeyFile_Client2.get_big_key_nonce(0, big_key_nonce[0])
+        if not File_Manipulation.file_integrity_check(key_nonce, key_nonce_sum):
+            integrity_failed_closing_protocol("Integrity fail.")
+        else:
+            if data_check(key_nonce) == "ok":
+                KeyFile_Client2.get_big_key_nonce(1, key_nonce)
+    elif len(big_key_nonce) == 2:
+        bkey_nonceC1 = DH_Algorithm_Client1.decrypt(Client1_conn.receiving())
+        bkey_nonceC2 = DH_Algorithm_Client2.decrypt(Client2_conn.receiving())
+        key_nonce_sumC1, key_nonceC1 = KeyFile_Client2.get_big_key_nonce(0, bkey_nonceC1)
+        key_nonce_sumC2, key_nonceC2 = KeyFile_Client2.get_big_key_nonce(0, bkey_nonceC2)
+        if not File_Manipulation.file_integrity_check(key_nonceC1, key_nonce_sumC1) or not File_Manipulation.file_integrity_check(key_nonceC2, key_nonce_sumC2):
+            integrity_failed_closing_protocol("Integrity fail.")
+        else:
+            if data_check(key_nonceC1) == "ok" or data_check(key_nonceC2) == "ok":
+                big_key_nonce.append((bkey_nonceC1 + bkey_nonceC2))
+                Client1_conn.sending(bkey_nonceC2)
+                Client2_conn.sending(bkey_nonceC1)
+                return False
+
+def receiving_sending_file(mode):
+    keyfile_reload()
+    if mode == 0:
+        crypted_file = Client1_conn.receiving()
+        AES_Encryption.update_data(crypted_file, KeyFile_Client1.key, KeyFile_Client1.nonce)
+        file_sum, uncrypted_file = File_Manipulation.get_file_information(AES_Encryption.decrypt())
+        if not File_Manipulation.file_integrity_check(uncrypted_file, file_sum):
+            integrity_failed_closing_protocol("Integrity fail.")
+        else:
+            if data_check(uncrypted_file) == "ok":
+                AES_Encryption.update_data(File_Manipulation.format_file(uncrypted_file, file_sum), KeyFile_Client2.key, KeyFile_Client2.nonce)
+                Client2_conn.sending(AES_Encryption.encrypt())
+    elif mode == 1:
+        crypted_file = Client2_conn.receiving()
+        AES_Encryption.update_data(crypted_file, KeyFile_Client2.key, KeyFile_Client2.nonce)
+        file_sum, uncrypted_file = File_Manipulation.get_file_information(AES_Encryption.decrypt())
+        if not File_Manipulation.file_integrity_check(uncrypted_file, file_sum):
+            integrity_failed_closing_protocol("Integrity fail.")
+        else:
+            if data_check(uncrypted_file) == "ok":
+                AES_Encryption.update_data(File_Manipulation.format_file(uncrypted_file, file_sum), KeyFile_Client1.key, KeyFile_Client1.nonce)
+                Client1_conn.sending(AES_Encryption.encrypt())
+def data_check(data):
+    if data == "ping":
+        ping_or_pong("pong")
+    elif data == "Integrity fail.":
+        total_disconnection()
+    else:
+        return "ok"
+
+def ping_or_pong(ping_pong):
+    keyfile_reload()
+    formatted_ping = File_Manipulation.SHA512_checksum_creation(ping_pong) + File_Manipulation.delimiter2 + ping_pong
+    AES_Encryption.update_data(formatted_ping, KeyFile_Client1.key, KeyFile_Client1.nonce)
+    Client1_conn.sending(AES_Encryption.encrypt())
+    AES_Encryption.update_data(formatted_ping, KeyFile_Client2.key, KeyFile_Client2.nonce)
+    Client2_conn.sending(AES_Encryption.encrypt())
+    if ping_pong == "ping":
+        ping_check()
+
+def ping_check():
+    keyfile_reload()
+    received_data1 = Client1_conn.receiving()
+    received_data2 = Client2_conn.receiving()
+    AES_Encryption.update_data(received_data1, KeyFile_Client1.key, KeyFile_Client1.nonce)
+    decrypted_data1 = AES_Encryption.decrypt().split(File_Manipulation.delimiter2)
+    AES_Encryption.update_data(received_data2, KeyFile_Client2.key, KeyFile_Client2.nonce)
+    decrypted_data2 = AES_Encryption.decrypt().split(File_Manipulation.delimiter2)
+    if not File_Manipulation.file_integrity_check(decrypted_data1[1], decrypted_data1[0]) or not File_Manipulation.file_integrity_check(decrypted_data2[1], decrypted_data2[0]):
+        integrity_failed_closing_protocol("Integrity fail.")
+    else:
+        if decrypted_data1[1] == "Integrity fail." or decrypted_data2[1] == "Integrity fail.":
+            total_disconnection()
+        elif decrypted_data1[1] != "pong" or decrypted_data2[1] != "pong":
+            integrity_failed_closing_protocol("Ping check failed.")
+
+def keyfile_reload():
+    global key_initialised
+    # Reset File_manipulation init
+    File_Manipulation.reset_init()
+    # Verify length of nonce and key and take one nonce for each objects
+    if not KeyFile_Client1.key_nonce_length_check() or not KeyFile_Client2.key_nonce_length_check():
+        key_initialised = False
+    KeyFile_Client1.nonce_choice()
+    KeyFile_Client2.nonce_choice()
+
+def integrity_failed_closing_protocol(error):
+    keyfile_reload(1)
+    AES_Encryption.update_data((File_Manipulation.SHA512_checksum_creation(error) + File_Manipulation.delimiter + error), KeyFile_Client1.key, KeyFile_Client1.nonce)
+    Client1_conn.sending(AES_Encryption.encrypt())
+    AES_Encryption.update_data((File_Manipulation.SHA512_checksum_creation(error) + File_Manipulation.delimiter + error), KeyFile_Client2.key, KeyFile_Client2.nonce)
+    Client2_conn.sending(AES_Encryption.encrypt())
+    total_disconnection()
+    print("contacting an administrator..")
+
+def total_disconnection():
+    global danger
+    Client1_conn.disconnecting()
+    Client2_conn.disconnecting()
+    danger = True
+
+while not danger:
+
+    if not c1_connected:
+        c1_connected = conn_s(1)
+    elif not c2_connected:
+        c2_connected = conn_s(2)
+    else:
+        if not dh_initialised:  # Initialise DH_algo key creation / send
+            dh_initialised, key_initialised = dh_init()
+        elif not key_initialised:  # Initialise Key creation / send
+            key_initialised = key_init()
+        else:
+            if receiving_sending_mode == 0:
+                receiving_sending_file(receiving_sending_mode)
+                receiving_sending_mode = 1
+            elif receiving_sending_mode == 1:
+                receiving_sending_file(receiving_sending_mode)
+                receiving_sending_mode = 0
---
+++ Server/Server2/Objects_Server.py
@@ -0,0 +1,190 @@
+import socket
+import select
+import string
+import random
+import re
+import os
+import pathlib
+import hashlib
+import rstr
+import tkinter
+import tkinter.filedialog
+import pyDH
+from Crypto.Cipher import AES
+from Crypto import Random
+
+class Server:
+    def __init__(self):
+        self.host = '127.0.0.1'
+        self.port_listening = 6800
+        self.whitelisted_client = ["172.16.1.42", "127.0.0.1", "192.168.0.33", "192.168.0.34", "172.16.1.19"]
+        self.socket = ""
+        self.message_content = b""
+
+    def server_activation(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((self.host, self.port_listening))
+        s.listen(2)
+        self.establishing_conn(s)
+
+    def establishing_conn(self, sock):
+        try:
+            clientconnect, clientinfo = sock.accept()
+            self.socket = sock
+            ip, port = clientconnect.getpeername()
+            if ip in self.whitelisted_client:  # Whitelist application
+                print(ip, " is connected on port : ", port)
+            else:
+                print("This client isn't whitelisted")
+                print("Closing connection..")
+                sock.close()
+                self.server_activation()
+        except (ConnectionRefusedError, OSError):
+            print("e")
+            self.server_activation()
+
+    def disconnecting(self):
+        self.socket.close()
+
+    def receiving(self):
+        try:
+            client_to_read, wlist, xlist = select.select([self.socket], [], [], 0.05)
+        except select.error:  # avoid error if there's no one to read
+            pass
+        else:
+            self.message_content = b""
+            self.message_content = self.socket.recv(16777216)
+            if self.message_content.decode() == "":
+                self.receiving()
+            else:
+                return self.message_content.decode()
+
+    def sending(self, data):
+        self.socket.sendall(str(data).encode())
+
+
+class File:
+    def __init__(self):
+        self.delimiter = "#&_#"
+
+    def get_file_information(self, file):
+        cuted_file = file.split(self.delimiter)
+        return cuted_file[0], cuted_file[1]
+
+    def format_file(self, data, sum):
+        return sum + self.delimiter + data
+
+    def SHA512_checksum_creation(self, file):
+        return hashlib.sha512(file).hexdigest()
+
+    def file_integrity_check(self, data, sum):
+        if hashlib.sha512(data).hexdigest() == sum:
+            return True
+        else:
+            return False
+
+
+class DH_algorithm:
+    def __init__(self):
+        self.engine = ""
+        self.public_key = ""
+        self.private_key = ""
+
+    def public_key_generator(self):
+        self.engine = pyDH.DiffieHellman()
+        self.public_key = self.engine.gen_public_key()
+        return self.public_key
+
+    def private_key_generator(self, friendkey):
+        self.private_key = self.engine.gen_shared_key(int(friendkey))
+
+    def encrypt(self, data):
+        cipher = AES.new(self.private_key.encode(), AES.MODE_OCB)
+        crypted_key, tag = cipher.encrypt_and_digest(data)
+        return crypted_key
+
+    def decrypt(self, data):
+        cipher = AES.new(self.private_key.encode(), AES.MODE_OCB)
+        uncrypted_key = cipher.decrypt(data)
+        return uncrypted_key
+
+
+class Key:
+    def __init__(self):
+        self.big_key_original = ""
+        self.big_nonce_original = ""
+        self.big_key_modified = ""
+        self.big_nonce_modified = ""
+        self.key = ""
+        self.nonce = ""
+        self.n_choice = 0
+        self.k_choice = 0
+        self.delimiter1 = "([-_])"
+        self.delimiter2 = ")-_-_("
+
+    def key_nonce_length_check(self):
+        if len(self.big_key_modified) == 32:
+            return False
+        elif len(self.big_nonce_modified) <= 512:
+            self.key_choice()
+            self.big_nonce_modified = self.big_nonce_original
+
+     # /!\ MODIFIER LE NONCE ET LE KEY CHOICE /!\
+    def nonce_choice(self):
+        if self.n_choice == 0:
+            self.n_choice = 1
+            self.nonce = self.big_nonce_modified[-128:-64]
+        elif self.n_choice == 1:
+            self.n_choice = 2
+            self.nonce = self.big_nonce_modified[:-64]
+        elif self.n_choice == 2:
+            self.n_choice = 0
+            self.nonce = self.big_nonce_modified[64:]
+
+    def key_choice(self):
+        if self.k_choice == 0:
+            self.k_choice = 1
+            self.key = self.big_key_modified[-32:]
+        elif self.k_choice == 1:
+            self.k_choice = 2
+            self.key = self.big_key_modified[:32]
+        elif self.k_choice == 2:
+            self.k_choice = 0
+            self.key = self.big_key_modified[-64:-32]
+    # /!\ MODIFIER LE NONCE ET LE KEY CHOICE /!\
+
+    def get_big_key_nonce(self, mode, data):
+        if mode == 0:
+            splitted_data = data.split(self.delimiter2)
+            checksum = splitted_data[0]
+            key_nonce = splitted_data[1]
+            return checksum, key_nonce
+        elif mode == 1:
+            data_splitted = data.split(self.delimiter1)
+            self.big_key_original = data_splitted[0]
+            self.big_key_modified = self.big_key_original
+            self.big_nonce_original = data_splitted[1]
+            self.big_nonce_modified = self.big_nonce_original
+
+
+class AES_Algorithm:
+    def __init__(self):
+        self.data = ""
+        self.key = ""
+        self.nonce = ""
+
+    def update_data(self, data, key, nonce):
+        self.data = data
+        self.key = key
+        self.nonce = nonce
+
+    def encrypt(self):
+        cipher = AES.new(self.key.encode(), AES.MODE_OCB, nonce=self.nonce)
+        crypted_data, tag = cipher.encrypt_and_digest(self.data)
+        return crypted_data
+
+    def decrypt(self):
+        cipher = AES.new(self.key.encode(), AES.MODE_OCB, nonce=self.nonce)
+        uncrypted_data = cipher.decrypt(self.data)
+        return uncrypted_data
